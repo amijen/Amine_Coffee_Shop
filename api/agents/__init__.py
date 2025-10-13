c6daae8cdf4b43c8e9345189bd34d6fc7133316d
Amine Sellami <aminesellami0110@gmail.com>
2025-10-13 11:04:13 +0200

    Update __init__.py

--- api/agents/__init__.py
+++ api/agents/__init__.py
@@ -3,3 +3,4 @@ from .guard_agent import GuardAgent
 from .classification_agent import ClassificationAgent
 from .details_agent import DetailsAgent
 from .agent_protocole import AgentProtocol
+from .order_taking_agent import OrderTakingAgent
